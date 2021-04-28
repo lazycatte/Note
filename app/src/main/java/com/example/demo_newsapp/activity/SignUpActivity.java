50fad8b26c15da119be075633969537656b6833a
Chaoyu <631479724@qq.com>
2021-04-28 12:10:16 +0800

    fix signup problem

--- app/src/main/java/com/example/demo_newsapp/activity/SignUpActivity.java
+++ app/src/main/java/com/example/demo_newsapp/activity/SignUpActivity.java
@@ -1,11 +1,9 @@
 package com.example.demo_newsapp.activity;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
