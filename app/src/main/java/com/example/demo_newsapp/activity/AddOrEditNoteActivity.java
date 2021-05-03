7adf18f3779c8d62d44356b530f2ce8060152357
xinliu-c <2323953781@qq.com>
2021-05-03 18:26:24 +0800

    Add note frontend and part of backlog

---
+++ app/src/main/java/com/example/demo_newsapp/activity/AddOrEditNoteActivity.java
@@ -0,0 +1,154 @@
+package com.example.demo_newsapp.activity;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Map;
+
+import data.dbHelper;
+
+public class AddOrEditNoteActivity extends Activity {
+
+    private ImageView mImgBack, mImgAdd;
+    private TextView mTvTitle;
+
+    private EditText et_no;
+    private EditText et_name;
+    private EditText et_ar;
+    private EditText et_pr;
+    private Button addBtn;
+
+    private static String DB_NAME = "mydb";
+    private data.dbHelper dbHelper;
+    private SQLiteDatabase db;
+
+    private String selId;
+    private Map<String, Object> listItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_edit_book);
+
+        if (getIntent() != null) {
+            listItem = (Map<String, Object>) getIntent().getSerializableExtra("listItem");
+        }
+
+        dbHelper = new dbHelper(this, DB_NAME, null, 1);
+        db = dbHelper.getWritableDatabase();
+
+        init();
+    }
+
+    private void init() {
+
+        mImgAdd = findViewById(R.id.imageView5);
+        mImgBack = findViewById(R.id.imageView4);
+        mTvTitle = findViewById(R.id.tv);
+        addBtn = findViewById(R.id.addBtn);
+
+        et_no = findViewById(R.id.et_no);
+        et_name = findViewById(R.id.et_name);
+        et_ar = findViewById(R.id.et_ar);
+        et_pr = findViewById(R.id.et_pr);
+
+        if (listItem == null) {
+            mTvTitle.setText("Add Note");
+            addBtn.setText("Add Note");
+        } else {
+            mTvTitle.setText("Update Note");
+            addBtn.setText("Update Note");
+            et_no.setText((String) listItem.get("bno"));
+            et_name.setText((String) listItem.get("bname"));
+            et_ar.setText((String) listItem.get("bar"));
+            et_pr.setText((String) listItem.get("bpr"));
+            selId = (String) listItem.get("_id");
+        }
+
+        mImgAdd.setVisibility(View.GONE);
+
+        mImgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listItem == null){
+                    dbAdd();
+                }else {
+                    dbUpdate();
+                }
+
+            }
+        });
+    }
+
+
+    protected void dbAdd() {
+
+        String bno = et_no.getText().toString().trim();
+        String bname = et_name.getText().toString().trim();
+        String bar = et_ar.getText().toString().trim();
+        String bpr = et_pr.getText().toString().trim();
+
+        if (TextUtils.isEmpty(bno) || TextUtils.isEmpty(bname) || TextUtils.isEmpty(bar) || TextUtils.isEmpty(bpr)) {
+            Toast.makeText(this, "Please complete!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO Auto-generated method stub
+        ContentValues values = new ContentValues();
+        values.put("bno", bno);
+        values.put("bname", bname);
+        values.put("bar", bar);
+        values.put("bpr", bpr);
+        long rowid = db.insert(dbHelper.TB_NAME, null, values);
+        if (rowid == -1)
+            Toast.makeText(this, "Fail to add!", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Successfully added!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    protected void dbUpdate() {
+
+        String bno = et_no.getText().toString().trim();
+        String bname = et_name.getText().toString().trim();
+        String bar = et_ar.getText().toString().trim();
+        String bpr = et_pr.getText().toString().trim();
+
+        if (TextUtils.isEmpty(bno) || TextUtils.isEmpty(bname) || TextUtils.isEmpty(bar) || TextUtils.isEmpty(bpr)) {
+            Toast.makeText(this, "Please complete!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO Auto-generated method stub
+        ContentValues values = new ContentValues();
+        values.put("bno", bno);
+        values.put("bname", bname);
+        values.put("bar", bar);
+        values.put("bpr", bpr);
+        String where = "_id=" + selId;
+        int i = db.update(dbHelper.TB_NAME, values, where, null);
+        if (i > 0) {
+            Toast.makeText(this, "Update succeeded!", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(this, "Update failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/demo_newsapp/activity/NoteInformationActivity.java
@@ -0,0 +1,171 @@
+package com.example.demo_newsapp.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import data.dbHelper;
+
+public class NoteInformationActivity extends Activity {
+
+    private ImageView mImgBack,mImgAdd;
+    private TextView mTvTitle;
+
+    private static String DB_NAME = "mydb";
+    private ArrayList<Map<String, Object>> data;
+    private data.dbHelper dbHelper;
+    private SQLiteDatabase db;
+    private Cursor cursor;
+    private SimpleAdapter listAdapter;
+    private View view;
+    private ListView listview;
+    private Map<String, Object> item;
+    private String selId;
+    private ContentValues selCV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_informatin);
+
+        init();
+
+        dbHelper = new dbHelper(this, DB_NAME, null, 1);
+        db = dbHelper.getWritableDatabase();
+        data = new ArrayList<>();
+
+
+    }
+    private void init() {
+
+        listview = findViewById(R.id.list_book);
+        mImgAdd = findViewById(R.id.imageView5);
+        mImgBack = findViewById(R.id.imageView4);
+        mTvTitle = findViewById(R.id.tv);
+
+        mTvTitle.setText("BelongToU");
+
+        mImgBack.setVisibility(View.GONE);
+
+        mImgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NoteInformationActivity.this,AddOrEditNoteActivity.class));
+            }
+        });
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // TODO Auto-generated method stub
+                Map<String, Object> listItem = (Map<String, Object>) listview.getItemAtPosition(position);
+                Intent intent = new Intent(NoteInformationActivity.this,AddOrEditNoteActivity.class);
+                intent.putExtra("listItem", (Serializable) listItem);
+                startActivity(intent);
+            }
+        });
+
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, Object> listItem = (Map<String, Object>) listview.getItemAtPosition(position);
+                selId = (String) listItem.get("_id");
+                dbDel();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        dbFindAll();
+    }
+
+
+    protected void dbDel() {
+        AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(this);
+        alertdialogbuilder.setTitle("HINT");
+        alertdialogbuilder.setMessage("Are you sure you want to delete this data?");
+        alertdialogbuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String where = "_id=" + selId;
+                int i = db.delete(dbHelper.TB_NAME, where, null);
+                if (i > 0){
+                    Toast.makeText(NoteInformationActivity.this,"Deleted successfully!",Toast.LENGTH_SHORT).show();
+                    dbFindAll();
+                }
+                else{
+                    Toast.makeText(NoteInformationActivity.this,"Delete failed!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        alertdialogbuilder.setNeutralButton("Cancel", null);
+        AlertDialog alertdialog1 = alertdialogbuilder.create();
+        alertdialog1.show();
+
+    }
+
+    private void showList() {
+        // TODO Auto-generated method stub
+        listAdapter = new SimpleAdapter(this, data,
+                R.layout.list_item, new String[]{"bno","bname","bar", "bpr"}, new int[]{R.id.tvNo, R.id.tvName, R.id.tvAr,R.id.tvPr,});
+        listview.setAdapter(listAdapter);
+    }
+
+
+
+    protected void dbFindAll() {
+        // TODO Auto-generated method stub
+        data.clear();
+        cursor = db.query(dbHelper.TB_NAME, null, null, null, null, null, "_id ASC");
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String id = cursor.getString(0);
+            String bno = cursor.getString(1);
+            String bname = cursor.getString(2);
+            String bar = cursor.getString(3);
+            String bpr = cursor.getString(4);
+            item = new HashMap<String, Object>();
+            item.put("_id", id);
+            item.put("bno", bno);
+            item.put("bname", bname);
+            item.put("bar", bar);
+            item.put("bpr", bpr);
+            data.add(item);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        showList();
+    }
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.setClass(NoteInformationActivity.this,MainActivity.class);
+        startActivity(intent);
+
+        NoteInformationActivity.this.finish();
+    }
+}
